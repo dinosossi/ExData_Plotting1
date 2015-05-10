50d3e40b3ecf2aaec39611824ec6ac06cc778be6
dinosossi <dino_sossi@yahoo.com>
2015-05-10 12:02:24 -0400

    Create plot1.R

---
+++ plot1.R
@@ -0,0 +1,11 @@
+mydata <- read.table("household_power_consumption.txt", header=TRUE, sep=";", na.strings = "?", nrows= 1000000, stringsAsFactors=FALSE)
+
+mydata$DateTime <- paste(mydata$Date, mydata$Time)
+
+mydata$DateTime <- as.Date(mydata$DateTime, format = "%d/%m/%Y %H:%M:%S")
+
+library(dplyr)
+
+subsetted <- filter(mydata, DateTime >= as.Date("2007-02-01 00:00:00"), DateTime < as.Date("2007-02-03 00:00:00"))
+
+hist(Global_active_power$Global_intensity, xlab="Global Active Power (kilowatts)", ylab="Frequency", main="Global Active Power", col="red")
